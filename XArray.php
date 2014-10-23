ab05daa51eebba9679bd2f6c37be52e19fe842a5
rudenich <rudenich@gmail.com>
2014-10-23 22:04:21 +0300

    corrections

--- XArray.php
+++ XArray.php
@@ -1,6 +1,35 @@
 <?
 if(!function_exists('array_column')){
-    require_once dirname(__FILE__).'/array_column.php';
+    if(!function_exists('array_column')){
+        function array_column ($input, $columnKey, $indexKey = null) {
+            if (!is_array($input)) {
+                return false;
+            }
+            if ($indexKey === null) {
+                foreach ($input as $i => &$in) {
+                    if (is_array($in) && isset($in[$columnKey])) {
+                        $in    = $in[$columnKey];
+                    } else {
+                        unset($input[$i]);
+                    }
+                }
+            } else {
+                $result    = array();
+                foreach ($input as $i => $in) {
+                    if (is_array($in) && isset($in[$columnKey])) {
+                        if (isset($in[$indexKey])) {
+                            $result[$in[$indexKey]]    = $in[$columnKey];
+                        } else {
+                            $result[]    = $in[$columnKey];
+                        }
+                        unset($input[$i]);
+                    }
+                }
+                $input    = &$result;
+            }
+            return $input;
+        }
+    }
 }
 
 class XArray{
@@ -50,6 +79,40 @@ class XArray{
                 $temp[$target] = isset($item[$source])?$item[$source]:null;
             }
             $res[] = $temp;
+        }
+        return $res;
+    }
+
+    /**
+     * Merges two or more arrays into one recursively.
+     * If each array has an element with the same string key value, the latter
+     * will overwrite the former (different from array_merge_recursive).
+     * Recursive merging will be conducted if both arrays have an element of array
+     * type and are having the same key.
+     * For integer-keyed elements, the elements from the latter array will
+     * be appended to the former array.
+     * @param array $a array to be merged to
+     * @param array $b array to be merged from. You can specify additional
+     * arrays via third argument, fourth argument etc.
+     * @return array the merged array (the original arrays are not changed.)
+     * @see mergeWith
+     */
+    public  function merge($a,$b)
+    {
+        $args=func_get_args();
+        $res=array_shift($args);
+        while(!empty($args))
+        {
+            $next=array_shift($args);
+            foreach($next as $k => $v)
+            {
+                if(is_integer($k))
+                    isset($res[$k]) ? $res[]=$v : $res[$k]=$v;
+                elseif(is_array($v) && isset($res[$k]) && is_array($res[$k]))
+                    $res[$k]=$this->merge($res[$k],$v);
+                else
+                    $res[$k]=$v;
+            }
         }
         return $res;
     }
